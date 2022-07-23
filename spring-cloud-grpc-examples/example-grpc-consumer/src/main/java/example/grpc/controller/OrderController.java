00d0dc2ed90539848ba553830e6eabc27aa11531
icodening <cleverbn96@gmail.com>
2022-07-23 16:07:49 +0800

    modify pom

--- spring-cloud-grpc-examples/example-grpc-consumer/src/main/java/example/grpc/controller/OrderController.java
+++ spring-cloud-grpc-examples/example-grpc-consumer/src/main/java/example/grpc/controller/OrderController.java
@@ -1,7 +1,10 @@
 package example.grpc.controller;
 
+import com.google.common.util.concurrent.ListenableFuture;
 import example.grpc.api.OrderServiceGrpc;
 import example.grpc.api.OrderServiceOuterClass;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
 import java.util.stream.Collectors;
 
 /**
@@ -21,13 +26,30 @@ import java.util.stream.Collectors;
 @RequestMapping("/orders")
 public class OrderController {
 
+    private static final Logger logger = LoggerFactory.getLogger(OrderController.class);
+
     @Autowired
     private OrderServiceGrpc.OrderServiceBlockingStub orderServiceBlockingStub;
+
+    @Autowired
+    private OrderServiceGrpc.OrderServiceFutureStub orderServiceFutureStub;
 
     @GetMapping("/{orderId}")
     public Object queryOrder(@PathVariable(name = "orderId") String orderId) {
         OrderServiceOuterClass.Query query = OrderServiceOuterClass.Query.newBuilder().setId(orderId).build();
         OrderServiceOuterClass.Order order = orderServiceBlockingStub.queryOrder(query);
+        ListenableFuture<OrderServiceOuterClass.Order> orderListenableFuture = orderServiceFutureStub.queryOrder(query);
+        orderListenableFuture.addListener(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    OrderServiceOuterClass.Order futureResult = orderListenableFuture.get();
+                    logger.info("queryOrder success via OrderServiceFutureStub: {}", OrderController.this.orderToMap(futureResult));
+                } catch (InterruptedException | ExecutionException e) {
+                    logger.error(e.getMessage(), e);
+                }
+            }
+        }, ForkJoinPool.commonPool());
         return orderToMap(order);
     }
 
