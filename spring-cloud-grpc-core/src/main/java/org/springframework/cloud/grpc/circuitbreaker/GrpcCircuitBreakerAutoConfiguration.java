b63832bebe9e3f23aa7a912ae381df29132352b7
icodening <cleverbn96@gmail.com>
2022-07-25 01:54:16 +0800

    auto configuration of sentinel grpc adapter

--- spring-cloud-grpc-core/src/main/java/org/springframework/cloud/grpc/circuitbreaker/GrpcCircuitBreakerAutoConfiguration.java
+++ spring-cloud-grpc-core/src/main/java/org/springframework/cloud/grpc/circuitbreaker/GrpcCircuitBreakerAutoConfiguration.java
@@ -1,8 +1,12 @@
 package org.springframework.cloud.grpc.circuitbreaker;
 
+import com.alibaba.cloud.sentinel.custom.SentinelAutoConfiguration;
+import com.alibaba.csp.sentinel.adapter.grpc.SentinelGrpcClientInterceptor;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JAutoConfiguration;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
 import org.springframework.cloud.grpc.circuitbreaker.resilience4j.Resilience4jGrpcCircuitBreakerFactory;
@@ -32,6 +36,17 @@ public class GrpcCircuitBreakerAutoConfiguration {
         @ConditionalOnBean(Resilience4JCircuitBreakerFactory.class)
         public GrpcCircuitBreakerFactory resilience4jGrpcCircuitBreakerFactory(Resilience4JCircuitBreakerFactory resilience4JCircuitBreakerFactory) {
             return new Resilience4jGrpcCircuitBreakerFactory(resilience4JCircuitBreakerFactory);
+        }
+    }
+
+    @ConditionalOnClass({SentinelGrpcClientInterceptor.class, SentinelAutoConfiguration.class})
+    @ConditionalOnProperty(name = "spring.cloud.sentinel.enabled", matchIfMissing = true)
+    public static class GrpcSentinelCircuitBreakerAutoConfiguration {
+
+        @Bean
+        @ConditionalOnMissingBean(GrpcCircuitBreakerInterceptor.class)
+        public SentinelGrpcClientInterceptor sentinelGrpcClientInterceptor() {
+            return new SentinelGrpcClientInterceptor();
         }
     }
 
