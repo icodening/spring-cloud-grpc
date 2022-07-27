e82cb98e571e0c317133e468e7161a71303f542c
icodening <cleverbn96@gmail.com>
2022-07-27 21:33:28 +0800

    get service name and method name via CallOptions

--- spring-cloud-grpc-core/src/main/java/org/springframework/cloud/grpc/circuitbreaker/GrpcCircuitBreakerInterceptor.java
+++ spring-cloud-grpc-core/src/main/java/org/springframework/cloud/grpc/circuitbreaker/GrpcCircuitBreakerInterceptor.java
@@ -11,8 +11,11 @@ import io.grpc.MethodDescriptor;
 import io.grpc.Status;
 
 import javax.annotation.Nullable;
+import java.util.Optional;
 
 import static org.springframework.cloud.grpc.support.GrpcCallOptions.APPLICATION;
+import static org.springframework.cloud.grpc.support.GrpcCallOptions.METHOD;
+import static org.springframework.cloud.grpc.support.GrpcCallOptions.SERVICE;
 
 /**
  * @author icodening
@@ -29,7 +32,9 @@ public class GrpcCircuitBreakerInterceptor implements ClientInterceptor {
     @Override
     public <ReqT, RespT> ClientCall<ReqT, RespT> interceptCall(MethodDescriptor<ReqT, RespT> method, CallOptions callOptions, Channel next) {
         String application = callOptions.getOption(APPLICATION);
-        GrpcCircuitBreaker grpcCircuitBreaker = grpcCircuitBreakerFactory.createGrpcCircuitBreaker(application, method.getFullMethodName());
+        String serviceName = Optional.ofNullable(callOptions.getOption(SERVICE)).orElse(method.getServiceName());
+        String methodName = Optional.ofNullable(callOptions.getOption(METHOD)).orElse(method.getBareMethodName());
+        GrpcCircuitBreaker grpcCircuitBreaker = grpcCircuitBreakerFactory.createGrpcCircuitBreaker(application, serviceName + "/" + methodName);
         ClientCall<ReqT, RespT> call = next.newCall(method, callOptions);
         return new ForwardingClientCall.SimpleForwardingClientCall<ReqT, RespT>(call) {
             @Override
